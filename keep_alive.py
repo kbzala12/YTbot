3bb1305f2b095aa8f14631d677d76db632af78cb
kbzala12 <zalakishorsinh51@gmail.com>
2025-08-25 09:59:51 +0530

    Update keep_alive.py

--- keep_alive.py
+++ keep_alive.py
@@ -1,19 +1,8 @@
 from flask import Flask
-from threading import Thread
-
-app = Flask('')
-
-@app.route('/')
+bot = Flask('')
+@bot.route('/')
 def home():
-    return "Bot is Running!"
+    return "Alive"
 
-def run():
-    app.run(host='0.0.0.0', port=8080)
-
-def keep_alive():
-    Thread(target=run).start()
-
-requirements.txt:
-
-pyTelegramBotAPI
-flask
+if __name__ == '__main__':
+    bot.run()
