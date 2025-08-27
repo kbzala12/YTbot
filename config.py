40df53f494c2887aef0c8b1ede87bc5b8e5c22c1
kbzala12 <zalakishorsinh51@gmail.com>
2025-08-27 07:36:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 import os
 
 # ---------------- Bot Credentials ----------------
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "7978191312:AAFFaOkxBSI9YoN4uR3I5FtZbfQNojT8F4U")  # Telegram Bot Token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "7978191312:AAHxYcBrnJbQ_ccc_IR3xSHaNIprnrpuEs4")  # Telegram Bot Token
 ADMIN_ID = int(os.environ.get("ADMIN_ID", 7459795138))  # Admin Telegram ID
 BOT_USERNAME = os.environ.get("BOT_USERNAME", "Bingyt_bot")  # Bot Username
 
